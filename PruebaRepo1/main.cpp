b7374a7c7104088440f0ddb761da27771b37c1f0
paulina.arregocesg@udea.edu.co <paulina.arregocesg@udea.edu.co>
2025-10-02 13:55:56 -0300

    Agregar otro comentario developer1

--- PruebaRepo1/main.cpp
+++ PruebaRepo1/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Hello World!" << endl;
     cout << "Esta es una prueba para Github" << endl;
     cout <<"Cambios por el desarrollador1";
+    cout <<"Agregar otro comentarios por desarrollador1";
     return 0;
 }
